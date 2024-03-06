35e05dd86d1ab784ff4b7abe488438ef655523e5
LeonardoViotti <leonardoviotti@gmail.com>
2024-03-06 09:17:20 -0500

    Multi-index for clips and num_positives argument working!

--- annotation.py
+++ annotation.py
@@ -8,55 +8,29 @@ import IPython.display as ipd
 import os
 import time
 
-# from utils import find_path
 
 #-----------------------------------------------------------------------------------------------------
-# Utils
-
-def find_path(file_name, path):
-    """ Searches [path] for [file_name]
-    Args:
-        file_name (str): File name with extention
-        path (str): Relative/Absolute search dir path
-
-    Returns:
-        (str): Absolute filepath for [file_name]
-    """
-    
-    files = []
-    for dir,_,_ in os.walk(path):
-        files.extend(glob(os.path.join(dir,file_name)))
-    
-    assert len(files) == 1, f"Multiple files found for {file_name}! {files}"
-    
-    return files[0]
+# Functions
 
 
-def create_relative_path(scores_df):
-    scores_df = scores_df.drop('Unnamed: 0', axis = 1)
-    scores_df['relative_path'] = scores_df['file'].str.split('/', expand = True).iloc[:,-2] + '/' + scores_df['clip']
-    return scores_df
-
-
-#-----------------------------------------------------------------------------------------------------
-# Core functions
-
-
-def plot_clip(clip_path,  directory = None, bandpass = [1, 10000], mark_at_s = None ):
+def plot_clip(audio_path,
+              st = None,
+              end = None,
+              bandpass = [1, 10000], 
+              mark_at_s = None ):
     """ Load file, display spectograms and play audio
-
+    
     Args:
-        clip_path (str): Audio file path
-        directory (str, optional): In case [clip_path] is not a full path, search [directory] for it. Defaults to None.
+        audio_path (str): Audio file path
+        directory (str, optional): In case [audio_path] is not a full path, search [directory] for it. Defaults to None.
         bandpass (list, Hz): Length 2 list specifying a frequency range to display. Format is [lower_band, higher_band].
         mark_at_s (list, s): List of seconds to add vertical lines in spectrogram. Typically used to mark start and end of valid clip.
     """
-    if directory is not None:
-        full_clip_path = find_path(clip_path, directory)
-    else:
-        full_clip_path = clip_path
-
-    audio = Audio.from_file(full_clip_path).bandpass(bandpass[0],bandpass[1],order=10)
+    
+    audio = Audio.from_file(audio_path).bandpass(bandpass[0],bandpass[1],order=10)
+    
+    if (st is not None) & (end is not None):
+        audio = audio.trim(st,end)
     
     # Add length markings 
     if mark_at_s is not None:
@@ -134,7 +108,7 @@ def save_annotations_file(annotations_df, scores_csv_path):
 
 def load_scores_df(scores_csv_path, 
                    annotation_column = 'annotation',
-                   index_column = 'relative_path',
+                   index_cols = 'relative_path',
                    notes_column = 'notes',
                    custom_annotation_column = None,
                    sort_by = None, 
@@ -145,10 +119,10 @@ def load_scores_df(scores_csv_path,
     Args:
         scores_csv_path (str): Relative or absolute file path to CSV scores data.
         annotation_column (str, optional): Annotation column name. Defaults to 'annotation'.
-        index_column (str, optional): Colum to set pd.DataFrame index by. Defaults to 'clip'.
+        index_cols (str, optional): Colum to set pd.DataFrame index by. Defaults to 'clip'.
         notes_column (str, optional): Annotation notes column name. Defaults to 'notes'.
         custom_annotation_column (str, optional): If there are custom annotations, column name. Defaults to 'additional_annotation'.
-        sort_by (str, optional): Columns to sort scores data by. Defaults to None.
+        sort_by (list, optional): Columns to sort scores data by. Defaults to None.
         dry_run (bool, optional): Not export outputs. Defaults to False.
     
     Returns:
@@ -157,17 +131,21 @@ def load_scores_df(scores_csv_path,
     # Load or create the annotations csv.
     try:
         scores_df = pd.read_csv(f"{scores_csv_path.split('.')[0]}_annotations.csv")
-        scores_df = scores_df.set_index(index_column)
+        scores_df = scores_df.set_index(index_cols)
         
         annotation_csv_exists = True
         
     except:
         scores_df = pd.read_csv(scores_csv_path)
-        scores_df = scores_df.set_index(index_column)
+        scores_df = scores_df.set_index(index_cols)
         
         # Create annotations columns
         scores_df[annotation_column] = np.NaN
         scores_df[notes_column] = np.NaN
+        
+        # # Testar essa porra
+        # scores_df['num_annotation'] = 0
+        # scores_df['cum_sum'] = 0
         
         if custom_annotation_column:
             scores_df[custom_annotation_column] = np.NaN
@@ -189,12 +167,14 @@ def annotate(audio_dir,
              valid_annotations = ["0", "1", "u"],
              scores_filename = "_scores.csv", 
              annotation_column = 'annotation',
-             path_column = 'relative_path',
+            #  path_column = 'relative_path',
+             index_cols = 'relative_path',
              notes_column = 'notes',
              custom_annotation_column = 'additional_annotation',
              skip_cols = None,
              n_positives = 1,
              mark_at_s = [0,10],
+            #  mark_at_s = None,
              sort_by = None, 
              date_filter = [], 
              card_filter = [], 
@@ -202,34 +182,41 @@ def annotate(audio_dir,
              n_sample = None,
              dry_run = False):
     """Loops through detection scores data that hasn't been annated and aks user to input annotations.
-
+    
     Args:
         audio_dir (str): Directory containing audio clips to be annotated.
         valid_annotations (list, optional): List of valid options for user. Defaults to ["0", "1", "u"].
         scores_filename (str, optional): Detection scores CSV filename. This function assumes it is in [audio_dir]. Defaults to "_scores.csv".
         annotation_column (str, optional): Annotation column name. Defaults to 'annotation'.
-        dates_filter (list (str), optional): List dates to be annotated (skip others). Defaults to empty list, [].
-        card_filter (list (str), optional): List cards to be annotated (skip others). Defaults to empty list, [].
-        custom_annotations_dict (dict, optional): _description_. Defaults to None.
+        index_cols (str, optional): Either ["path_to_clip"] or ["path_to_audio", "clip_start_time", "clip_end_time"]. Defaults to 'relative_path'.
+        notes_column (str, optional): Column name for notes. Defaults to 'notes'.
+        custom_annotation_column (str, optional): Column name for additional annotation. Defaults to 'additional_annotation'.
         skip_cols (str, optional): Column names for skipping clips if a positive clip already flagged.
         n_positives (int, optional): Number of positives needed before skipping if skip_cols is provided. Defaults to 1.
+        mark_at_s (list, optional): Seconds to mark clip with vertical lines. Usually to define start and end of clip if there is padding. Defaults to None.
+        sort_by (list, optional): Columns to sort scores data by. Defaults to None.
+        date_filter (list (str), optional): List dates to be annotated (skip others). Defaults to empty list, [].
+        card_filter (list (str), optional): List cards to be annotated (skip others). Defaults to empty list, [].
+        custom_annotations_dict (dict, optional): _description_. Defaults to None.
         n_sample (int, optional): Sample from valid rows. Defaults to None.
         dry_run (bool, optional):  Not export outputs. Defaults to False.
     
+    Exports:
+        Every iteration exports a file named [scores_filename]_annotations.csv to [audio_dir] 
+        
     Returns:
-        _type_: _description_
+        pd.DataFrame with annotations
     """
     
     scores_csv_path = os.path.join(audio_dir, scores_filename)
     
     scores_df, annotation_csv_exists = load_scores_df(scores_csv_path,
                                annotation_column = annotation_column,
-                               index_column = path_column,
+                               index_cols = index_cols,
                                notes_column = notes_column,
                                custom_annotation_column = custom_annotation_column,
                                sort_by = sort_by, 
                                dry_run = dry_run)
-    # scores_df = scores_df.set_index(path_column)
     
     # Skip of data ot card filter provided
     if date_filter or card_filter:
@@ -238,12 +225,6 @@ def annotate(audio_dir,
         scores_df['skip'] = False
     
     # Skip if skip_if_present columns provided.
-    
-    # Create absolute path index
-    # scores_df['absolute_path'] = audio_dir + '/' + scores_df['relative_path']
-    scores_df['absolute_path'] = audio_dir + '/' + scores_df.index
-    
-    # scores_df = scores_df.set_index('absolute_path')
     
     valid_rows = scores_df[~scores_df[annotation_column].notnull()]
     if n_sample is not None:
@@ -276,28 +257,33 @@ def annotate(audio_dir,
             scores_df.at[idx, annotation_column] = "not reviewed"
         else:
             print(f"Clip: {idx}")
-        
-            # plot_clip(idx, mark_at_s = [3, 7])
-            plot_clip(row['absolute_path'], mark_at_s = mark_at_s)
+            
+            if len(idx) == 1: # Assume it is a path for an already trimed clip
+                plot_clip(idx, mark_at_s = mark_at_s)
+            elif len(idx) == 3:
+                plot_clip(idx[0], idx[1], idx[2], mark_at_s = mark_at_s)
+            else:
+                raise Exception('index_cols must be either ["path_to_clip"] or ["path_to_audio", "clip_start_time", "clip_end_time"]')
+            
             time.sleep(.1) # Added delay for stability (hopefully)
             annotations = user_input(valid_annotations, 
                                      custom_annotations_dict = custom_annotations_dict, positive_annotation = '1')
 
-            scores_df.at[idx, annotation_column] = annotations[0]
-            scores_df.at[idx, custom_annotation_column] = annotations[1]
-            scores_df.at[idx, notes_column]= annotations[2]
+            scores_df.loc[idx, annotation_column] = annotations[0]
+            scores_df.loc[idx, custom_annotation_column] = annotations[1]
+            scores_df.loc[idx, notes_column]= annotations[2]
             
             if skip_cols:
                 assert set(skip_cols).issubset(scores_df.columns), "skip_cols not present!"
                 assert isinstance(skip_cols, list), f'skip_cols argument must be a list!'
                 
                 # Update the cumulative sum every iteration
-                scores_df['num_annotation'] =  pd.to_numeric(scores_df[annotation_column], errors='coerce').fillna(0).astype(int)
+                # scores_df['num_annotation'] =  pd.to_numeric(scores_df[annotation_column], errors='coerce').fillna(0).astype(int)
+                scores_df['num_annotation'] =  (~scores_df[annotation_column].isna()).astype(int)
                 scores_df['cum_sum'] = scores_df.groupby(skip_cols)['num_annotation'].cumsum()
                 
-                
                 # if scores_df.at[idx, annotation_column] == '1':
-                if scores_df.at[10, 'cum_sum'] > n_positives:
+                if (scores_df.loc[idx, 'cum_sum'] > n_positives).item():
                     # Create bolean series if row equal to current value of skip col
                     skip_bool_list = []
                     for skip_col in skip_cols:
@@ -312,17 +298,14 @@ def annotate(audio_dir,
                     skip_bool_series = pd.concat(skip_bool_list, axis=1).all(axis=1)
                     
                     scores_df.loc[skip_bool_series,annotation_column] = 'skipped'
-            
-        # print(scores_df)
-        # time.sleep(10) # Added delay for stability (hopefully)
-        
         if not dry_run: 
-            # save_annotations_file(scores_df.drop(['skip', 'absolute_path'], axis = 1), scores_csv_path)
-            # scores_df = scores_df.reset_index()
-            save_annotations_file(scores_df.drop(['skip', 'absolute_path', 'num_annotation', 'cum_sum'], axis = 1), scores_csv_path)
-        
+            save_annotations_file(scores_df.drop(['skip', 'num_annotation', 'cum_sum'], axis = 1), scores_csv_path)
+            # save_annotations_file(scores_df.drop(['skip'], axis = 1), scores_csv_path)
         # Update params
         n_clips_remaining = len(scores_df[~scores_df[annotation_column].notnull()])
         valid_rows = scores_df[~scores_df[annotation_column].notnull()]
     
     return scores_df
+
+
+# if __name__ == "__main__":
